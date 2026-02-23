64ba63e53e7c96c5f8aeccb390418d369a9336d5
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-02-23 23:51:19 +0530

    added Binary Search

--- 07-LinearSearch/src/com/LinearSearch/TwoSum.java
+++ 07-LinearSearch/src/com/LinearSearch/TwoSum.java
@@ -9,7 +9,7 @@ public class TwoSum {
     }
      static int[] twoSum(int[] nums, int target) {
          for (int i = 0; i < nums.length; i++) {
-             for (int j = 1; j < nums.length - 1; j++) {
+             for (int j = i + 1; j < nums.length - 1; j++) {
                  if (nums[i] + nums[j] == target){
                      return new int[]{i,j};
                  }
---
+++ 08-BinarySearch/src/com/BinarySearch/BinarySearch.java
@@ -0,0 +1,10 @@
+package com.BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,9,11,12,14,20,36,48};
+    }
+    static void binarySearch(int[] arr){
+        
+    }
+}
