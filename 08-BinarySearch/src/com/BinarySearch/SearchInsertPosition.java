14e04c38f172f3c1c078c8665ff4136b49e60940
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-02-26 14:23:10 +0530

    added search insert position

---
+++ 08-BinarySearch/src/com/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,24 @@
+package com.BinarySearch;
+
+public class SearchInsertPosition {
+   public static void main(String[] args) {
+        int[] arr = {1,3,5,6};
+       System.out.println(searchInsert(arr,4));
+
+    }
+    static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if (target == nums[mid]){
+                return mid;
+            }else if (target < nums[mid]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
