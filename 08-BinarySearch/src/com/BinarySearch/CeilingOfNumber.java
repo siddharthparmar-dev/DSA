3f3e3e39b665f391846f69d44bfcc7b3b61ac037
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-02-27 22:02:48 +0530

    Added first last position in sorted array

--- 08-BinarySearch/src/com/BinarySearch/CeilingOfNumber.java
+++ 08-BinarySearch/src/com/BinarySearch/CeilingOfNumber.java
@@ -1,23 +1,27 @@
 package com.BinarySearch;
 
+import java.util.Arrays;
+
 public class CeilingOfNumber {
     public static void main(String[] args) {
-        int[] arr = {2,3,5,9,14,16,18};
-        System.out.println(ceiling(arr,13));
+//        int[] arr = {2,3,5,9,14,16,18};
+        int[] arr = {3,4,4,7,8,10};
+        int[] ans = ceiling(arr,5);
+        System.out.println(Arrays.toString(ans));
     }
-    static int ceiling(int[] arr , int target){
+    static int[] ceiling(int[] arr , int target){
         int start = 0;
         int end = arr.length-1;
         while (start <= end){
             int mid = start + (end - start) / 2;
             if (target == arr[mid]){
-                return arr[mid];
+                return new int[] {arr[mid]};
             }else if (target > arr[mid]){
                 start = mid + 1;
             }else {
                 end = mid - 1;
             }
         }
-        return arr[start];
+        return new int[] {arr[end],arr[start]};
     }
 }
---
+++ 08-BinarySearch/src/com/BinarySearch/FirstLastPosition.java
@@ -0,0 +1,56 @@
+package com.BinarySearch;
+
+import java.util.Arrays;
+
+public class FirstLastPosition {
+
+    public static void main(String[] args) {
+        int[] arr = {5,7,7,7,8,9};
+        int[] ans = {first(arr,7), last(arr,7)};
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int first(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target == nums[mid]){
+                ans = mid;
+                end = mid - 1;   // move left
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static int last(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target == nums[mid]){
+                ans = mid;
+                start = mid + 1;   // move right
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
