0525ecdbdaa2bdd966b5581750e98566b543e95c
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-03-03 00:42:46 +0530

    added search in rotated sorted array

--- 08-BinarySearch/src/com/BinarySearch/RotatedSortedArray.java
+++ 08-BinarySearch/src/com/BinarySearch/RotatedSortedArray.java
@@ -2,27 +2,27 @@ package com.BinarySearch;
 
 public class RotatedSortedArray {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2,5};
-        int peak = peakIndexInMountainArray(arr);
-        int target = 5;
+        int[] arr = {5,1,3};
+        int peak = pivotElement(arr);
+        System.out.println(peak);
+        int target = 3;
         int leftSide = search(arr,target,0,peak);
         if (leftSide != -1){
             System.out.println(leftSide);
         }
         int rightSide = search(arr,target,peak + 1,arr.length -1);
+        System.out.println(rightSide);
     }
-    static int peakIndexInMountainArray(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end){
-            int mid = start + (end - start) / 2;
-            if (arr[mid] > arr[mid + 1]){
-                end = mid;
-            }else {
-                start = mid + 1;
+    static int pivotElement(int[] arr) {
+        int max = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+                index = i;
             }
         }
-        return start;
+        return index;
     }
 
     static int search(int[] nums, int target,int start,int end) {
