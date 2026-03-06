fb776cb5d00cd12dcdf65cc5a2f2f7fcd6274a77
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-03-06 20:29:04 +0530

    added Minimum in rotated sorted array

---
+++ 08-BinarySearch/src/com/BinarySearch/MinimumInRotatedSorted.java
@@ -0,0 +1,25 @@
+package com.BinarySearch;
+
+public class MinimumInRotatedSorted {
+    public static void main(String[] args) {
+        int[] arr = {6,7,8,9,0,1,2,3,4,5};
+        System.out.println(minimum(arr));
+    }
+    static int minimum(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if (arr[start] <= arr[end]){
+                ans = Math.min(ans,arr[start]);
+                start = mid + 1;
+            }
+            else {
+                ans = Math.min(ans,arr[mid]);
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
