1061fb14763cf6235fab8911c5f4695f41f9cd7f
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-03-10 23:56:54 +0530

    added Minimum in Rotated sorted Array II

---
+++ 08-BinarySearch/src/com/BinarySearch/MinimumInRotatedSortedII.java
@@ -0,0 +1,26 @@
+package com.BinarySearch;
+
+public class MinimumInRotatedSortedII {
+    public static void main(String[] args) {
+        int[] nums = {2,2,2,0,1};
+        System.out.println(findMin(nums));
+    }
+    static int findMin(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end){
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[end])
+            {
+                start = mid + 1;
+            }else if(arr[mid] < arr[end])
+            {
+                end = mid;
+            }else
+            {
+                end--;
+            }
+        }
+        return arr[start];
+    }
+}
