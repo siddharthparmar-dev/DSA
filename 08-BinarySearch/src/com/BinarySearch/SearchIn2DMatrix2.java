b06734df300ad0e3a30484165c9ae3e59a669531
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-03-19 19:55:47 +0530

    Added Search in 2D Matrix II

---
+++ 08-BinarySearch/src/com/BinarySearch/SearchIn2DMatrix2.java
@@ -0,0 +1,26 @@
+package com.BinarySearch;
+
+public class SearchIn2DMatrix2 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}
+        };
+        System.out.println(searchMatrix(arr,5));
+    }
+    static boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+}
