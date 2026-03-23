a3b3135b338627064d4cde2c45f1945e4ab74c7f
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-03-23 21:13:28 +0530

    added Bubble sort

--- 07-Sorting/src/com/Sorting/BubbleSort.java
+++ 07-Sorting/src/com/Sorting/BubbleSort.java
@@ -1,8 +1,24 @@
 package com.Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-
+        int [] arr ={5,3,4,6,2};
+        arr = bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    
+    static int[] bubbleSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n-i; j++) {
+                if (arr[j-1] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
