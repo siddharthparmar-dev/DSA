0d3b67f280ebd243e0a51195961c8b0500c0d535
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-17 23:01:44 +0530

    added Number disappeared in array

---
+++ 07-Sorting/src/com/Sorting/NumbersDisappearedInArray.java
@@ -0,0 +1,19 @@
+package com.Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumbersDisappearedInArray {
+    static void main(String[] args) {
+
+    }
+    static List<Integer> findDisappearedNumbers(int[] nums) {
+        int  n = nums.length;
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+       while (i < n){
+           int correctIndex = nums[i] - 1;
+           if (nums[i]  )
+       }
+    }
+}
