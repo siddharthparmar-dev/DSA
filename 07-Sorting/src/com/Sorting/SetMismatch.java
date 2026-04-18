3229bc268227f6f1f1b65929da6006c3dccd7765
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-18 23:22:12 +0530

    Added find the duplicate number

--- 07-Sorting/src/com/Sorting/SetMismatch.java
+++ 07-Sorting/src/com/Sorting/SetMismatch.java
@@ -1,10 +1,33 @@
 package com.Sorting;
 
+import java.util.Arrays;
+
 public class SetMismatch {
     public static void main(String[] args) {
-
+        int[] arr = {1,2,2,4};
+        System.out.println(Arrays.toString(findErrorNums(arr)));
     }
     static int[] findErrorNums(int[] nums){
          int n = nums.length;
+         int i = 0;
+         while (i < n){
+             int correctIndex = nums[i] - 1;
+             if (nums[i] != nums[correctIndex]){
+                 int temp = nums[i];
+                 nums[i] = nums[correctIndex];
+                 nums[correctIndex] = temp;
+             }
+             else{
+                 i++;
+             }
+         }
+
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j+1){
+                return new int[] {nums[j],j+1};
+            }
+        }
+
+        return nums;
     }
 }
