9d1aaa74fc5c3266a935567cc159dcf15d23715c
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-18 21:15:57 +0530

    Added find the duplicate number

--- 07-Sorting/src/com/Sorting/FindTheDuplicateNumber.java
+++ 07-Sorting/src/com/Sorting/FindTheDuplicateNumber.java
@@ -2,7 +2,8 @@ package com.Sorting;
 
 public class FindTheDuplicateNumber {
     public static void main(String[] args) {
-
+        int[] arr = {1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
     }
     static int findDuplicate(int[] nums) {
         int  n = nums.length;
@@ -18,9 +19,12 @@ public class FindTheDuplicateNumber {
                 i++;
             }
         }
-
+        int duplicateNumber = -1;
         for (int j = 0; j < n; j++) {
-            
+            if (nums[j] != j){
+                duplicateNumber = nums[j];
+            }
         }
+        return duplicateNumber;
     }
 }
---
+++ 07-Sorting/src/com/Sorting/SetMismatch.java
@@ -0,0 +1,10 @@
+package com.Sorting;
+
+public class SetMismatch {
+    public static void main(String[] args) {
+
+    }
+    static int[] findErrorNums(int[] nums){
+         int n = nums.length;
+    }
+}
