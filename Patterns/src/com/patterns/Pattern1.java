7107d846de8cd9c4638d147904c2e4921c58aeab
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-27 00:14:23 +0530

    added pattern ques 1

---
+++ Patterns/src/com/patterns/Pattern1.java
@@ -0,0 +1,23 @@
+package com.patterns;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        pattern2(5);
+
+        /* o/p:
+         * * * * *
+         * * * * *
+         * * * * *
+         * * * * *
+         * * * * *
+         */
+    }
+    static void pattern2(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
--- Patterns/src/com/patterns/pattern.java
+++ Patterns/src/com/patterns/pattern.java
@@ -2,14 +2,19 @@ package com.patterns;
 
 public class pattern {
     public static void main(String[] args) {
-
+        pattern1(4);
     }
     static void pattern1(int n){
-        for (int i = 0; i < n; i++) {
-
+        for (int row = 1; row <= n; row++) {
+            //for every row run the column
+            for (int column = 1; column <= row ; column++) {
+                System.out.print("* ");
+            }
+            // when 1 row is printed, we need to add new line
+            System.out.println();
         }
     }
 }
 /*
-no of lines = no of rows
+no of lines = no of rows = no of outer for loop
  */
