b736e72519c73d791330d03795eaa7651528341d
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-29 00:18:25 +0530

    added pattern ques no 13

---
+++ 11-Patterns/src/com/patterns/Pattern13.java
@@ -0,0 +1,22 @@
+package com.patterns;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        pattern17(4);
+    }
+    static void pattern17(int n){
+        for (int row = 1; row < 2*n ; row++) {
+            int totalRowsInCols = row <= n ? row : 2*n - row;
+            for (int space = 0; space < n-totalRowsInCols; space++) {
+                System.out.print(" ");
+            }
+            for (int col = totalRowsInCols; col >= 1 ; col--) {
+                System.out.print(col);
+            }
+            for (int col = 2; col <= totalRowsInCols; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
