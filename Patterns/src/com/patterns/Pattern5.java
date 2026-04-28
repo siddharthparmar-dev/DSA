bdcba553f8e2f0f99142c28981de889d77f5b528
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-28 16:19:54 +0530

    added pattern ques no 5

--- Patterns/src/com/patterns/Pattern5.java
+++ Patterns/src/com/patterns/Pattern5.java
@@ -5,9 +5,15 @@ public class Pattern5 {
         pattern5(5);
     }
     static void pattern5(int n){
-        for (int row = 1; row <= n; row++) {
+        for (int row = 1; row <= 2*n; row++) {
+            if (row <= n){
             for (int col = 1; col <= row ; col++) {
                 System.out.print("* ");
+            }
+            }else {
+                for (int col = row+1; col <= 2*n ; col++) {
+                    System.out.print("* ");
+                }
             }
             System.out.println();
         }
