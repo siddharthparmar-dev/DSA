214c7cfc853d164ab2cf49941dfb86b1e3dfafda
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-04-30 21:06:44 +0530

    added fibonacci using recursion

---
+++ 12-Recursion/src/com/recursion/FibonacciUsingRecursion.java
@@ -0,0 +1,13 @@
+package com.recursion;
+
+public class FibonacciUsingRecursion {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(7));
+    }
+    static int fibonacci(int n){
+        if (n < 2){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
