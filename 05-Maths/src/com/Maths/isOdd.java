dc0bdf9bed9d98eba8990b7469cf28becb39c3a2
Siddharth <siddharthparmar.dev@gmail.com>
2026-06-01 16:56:08 +0530

    added isOdd

---
+++ 05-Maths/src/com/Maths/isOdd.java
@@ -0,0 +1,13 @@
+package com.Maths;
+
+public class isOdd {
+    public static void main(String[] args) {
+        System.out.println(isOdd(2));
+    }
+    static String isOdd(int n){
+        if ((n & 1) == 1){
+            return "odd";
+        }
+        return "Even";
+    }
+}
