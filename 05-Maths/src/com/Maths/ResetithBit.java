d5e79e32c8f07884ece8fa22d2f2f7d777322498
Siddharth <siddharthparmar.dev@gmail.com>
2026-06-01 18:56:07 +0530

    added Reset ith bit and set ith bit

---
+++ 05-Maths/src/com/Maths/ResetithBit.java
@@ -0,0 +1,13 @@
+package com.Maths;
+
+public class ResetithBit {
+    public static void main(String[] args) {
+        int n = 87;
+        System.out.println(Integer.toBinaryString(n));
+        int res = setBit(n,4);
+        System.out.println(Integer.toBinaryString(res));
+    }
+    static int setBit(int n,int i){
+        return n ^ (1 << (i - 1));
+    }
+}
