3c108b7b515e2c1329b1aeb2b0dd1397d54a4acc
Siddharth <siddharthparmar.dev@gmail.com>
2026-07-23 06:04:16 +0530

    Mode of array

--- 07-Arrays/src/com/Arrays/ModeOfArray.java
+++ 07-Arrays/src/com/Arrays/ModeOfArray.java
@@ -1,11 +1,12 @@
 package com.Arrays;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ModeOfArray {
     static void main(String[] args) {
         int[] arr = {1,2,2,4,5,2,7,5,7,2,3,7,5,1,9,2};
-        mode(arr);
+        mode2(arr);
     }
     static void mode(int[] arr){
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -18,6 +19,24 @@ public class ModeOfArray {
             if(max < map.get(i)){
                 max = map.get(i);
                 mode = i;
+            }
+        }
+        System.out.println("Mode of the array is " + mode);
+    }
+
+    static void mode2(int[] arr){
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i : arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int mode = 0;
+        int max = Integer.MIN_VALUE;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+
+            if(entry.getValue() > max){
+                max = entry.getValue();
+                mode = entry.getKey();
             }
         }
         System.out.println("Mode of the array is " + mode);
