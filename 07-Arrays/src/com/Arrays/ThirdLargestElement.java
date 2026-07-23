334f1809673716d61c0286706c6f5ea263116cfc
Siddharth <siddharthparmar.dev@gmail.com>
2026-07-24 01:43:59 +0530

    Added third largest element

--- 07-Arrays/src/com/Arrays/ThirdLargestElement.java
+++ 07-Arrays/src/com/Arrays/ThirdLargestElement.java
@@ -2,7 +2,7 @@ package com.Arrays;
 
 public class ThirdLargestElement {
     static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6};
+        int[] arr = {7,4,5,2,9,4,1,8,2,5,1,5,5,2,4,12,5,1,5,15,25,51,5,1,0,4};
         System.out.println(thirdMax(arr));
 
     }
@@ -16,6 +16,7 @@ public class ThirdLargestElement {
                 secondLargest = largest;
                 largest = num;
             } else if (num > secondLargest && num != largest) {
+                thirdLargest = secondLargest;
                 secondLargest = num;
             }else if(num > thirdLargest && num != largest &&  num != secondLargest){
                 thirdLargest = num;
