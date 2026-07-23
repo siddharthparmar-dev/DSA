bb5f1a023bd6f8b791c8444bb03b64d7bf58dd1f
Siddharth <siddharthparmar.dev@gmail.com>
2026-07-23 20:54:17 +0530

    Added Mode of array

--- 07-Arrays/src/com/Arrays/SecondLargestElement.java
+++ 07-Arrays/src/com/Arrays/SecondLargestElement.java
@@ -1,6 +1,6 @@
 package com.Arrays;
 
-public class MaximumElement {
+public class SecondLargestElement {
     public static void main(String[] args) {
         int[] nums = {4,8,7,9,2,1,2,7,8,3,8};
         System.out.println(maximumElement(nums));
